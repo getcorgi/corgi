533a98a8273eafa478ae19696412c921678e539f
orrybaram <orrybaram@gmail.com>
2020-03-31 20:33:25 -0400

    feat(app): add theme provider and dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
@@ -11,18 +12,26 @@ import { FirebaseProvider } from './components/Firebase';
 import Header from './components/Header';
 import { firebaseConfig } from './constants';
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
 const App: React.FC = () => {
   return (
     <>
-      <CssBaseline />
-      <FirebaseProvider config={firebaseConfig}>
-        <Router>
-          <Header />
-          <Route exact path="/" component={Boards} />
-          <Route exact path="/boards/:boardId" component={Board} />
-          <Route exact path="/error" component={ErrorPage} />
-        </Router>
-      </FirebaseProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <FirebaseProvider config={firebaseConfig}>
+          <Router>
+            <Header />
+            <Route exact path="/" component={Boards} />
+            <Route exact path="/boards/:boardId" component={Board} />
+            <Route exact path="/error" component={ErrorPage} />
+          </Router>
+        </FirebaseProvider>
+      </ThemeProvider>
     </>
   );
 };
