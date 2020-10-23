98c0aeb2fb583f2c56b71db280a1c7840cbd662b
Ben Budnevich <ben.bj32001@gmail.com>
2020-10-23 13:49:33 -0400

    chore: upgrade react-scripts

--- packages/eslint-config-corgi/index.js
+++ packages/eslint-config-corgi/index.js
@@ -11,7 +11,9 @@ module.exports = {
     'prettier',
   ],
   rules: {
-    '@typescript-eslint/ban-ts-ignore': 'warn',
+    '@typescript-eslint/prefer-as-const': 'warn',
+    '@typescript-eslint/ban-types': 'warn',
+    '@typescript-eslint/ban-ts-comment': 'warn',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
     'prettier/prettier': 'error',
