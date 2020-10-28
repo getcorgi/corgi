50f10112685782ed4ca8a1868c5ebac7a4c9000b
Maggie <mmh332@nyu.edu>
2020-10-27 23:10:05 -0400

    feat(giphySearch): create giphy search component

---
+++ packages/client/src/components/Group/components/Chat/components/GiphySearch/GiphySearch.styles.ts
@@ -0,0 +1,17 @@
+import { styled } from '@material-ui/core';
+
+export const GiphySearch = styled('div')({
+  cursor: 'pointer',
+  color: '#888a96',
+  '&:hover': {
+    color: '#ffca00',
+  },
+});
+
+export const GiphyWrapper = styled('div')({
+  height: '380px',
+});
+
+export const SearchBar = styled('div')({
+  border: '3px solid red',
+});
